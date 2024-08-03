4dd462aa838bccb2d356769946473600a57a838a
elussich <elussich@gmail.com>
2024-08-03 18:01:29 -0300

    Update to Vue 3 and Vite.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
-import Vue from 'vue'
+import { createApp, ref } from 'vue'
 import App from './App.vue'
 
 import "./assets/styles/index.css";
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).mount('#app')
